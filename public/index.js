da168dca2a10b29ad5e7b109996dd9e788d39b5e
Kevin <kevinwshin@utexas.edu>
2015-05-21 02:24:00 -0500

    added channel switching, testing next, then implementing real channel selection

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+var socket = io();
+
 socket.on('changeChannel', function(name) {
     $('#stream').attr('src', 'http://www.twitch.tv/' + name + '/embed');
     $('#chat').attr('src', 'http://www.twitch.tv/' + name + '/chat?popout=');
--- server.node.js
+++ server.node.js
@@ -5,11 +5,21 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var cacheTimeout = 24 * 60 * 60 * 1000;
-var currentChannel = 'scarra';
+var channelDuration = 60 * 1000;
+
+var pickChannel = function() {
+    return (currentChannel === 'finestko') ? 'sirhcez' : 'finestko';
+};
+var currentChannel = pickChannel;
+var changeChannel = function() {
+    currentChannel = pickChannel();
+    io.emit('changeChannel', currentChannel);
+};
 
 io.on('connection', function(socket) {
     socket.emit('changeChannel', currentChannel);
 });
+setInterval(changeChannel, channelDuration);
 
 app.use(compression());
 app.use(express.static(__dirname + '/public', { maxAge: cacheTimeout }));
