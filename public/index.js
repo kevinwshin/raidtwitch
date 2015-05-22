f11ae8067d3ecaa9e0c54b53f8725df41081a8e2
Kevin <kevinwshin@utexas.edu>
2015-05-22 06:49:51 -0500

    added the clock below the player using flexbox

--- public/index.js
+++ public/index.js
@@ -1,6 +1,14 @@
 var socket = io();
 
 socket.on('changeChannel', function(name) {
+    socket.emit('unvote');
     $('#stream').attr('src', 'http://www.twitch.tv/' + name + '/embed');
     $('#chat').attr('src', 'http://www.twitch.tv/' + name + '/chat?popout=');
+});
+
+socket.on('time', function(time) {
+    var minutes = Math.floor(time / 60);
+    var seconds = time - minutes * 60;
+    var timeString = minutes + (seconds < 10 ? ':0' : ':') + seconds;
+    $('#clock').text(timeString);
 });
