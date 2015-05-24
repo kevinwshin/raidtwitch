a85eeac57325f6c7e1da53dada39ae9ed5077b97
Kevin <kevinwshin@utexas.edu>
2015-05-24 00:29:00 -0500

    better initialization and checks that a stream is still live before changing

--- server.node.js
+++ server.node.js
@@ -19,7 +19,7 @@ var cacheTimeout = 24 * 60 * 60 * 1000; //ms
 var channelDuration = 4 * 60; //sec
 var maxPages = 7;
 
-var channelOffset = 5000;
+var channelOffset;
 var currentChannel;
 var channelList;
 var elapsedTime = 0;
@@ -27,33 +27,42 @@ var numConnected = 0;
 var upVotes = 0;
 var downVotes = 0;
 
+//utility to get the finished data from https.get
+var httpsGet = function(url, callback) {
+    https.get(url, function(res) {
+        var responseData = '';
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+            responseData += chunk;
+        });
+        res.on('end', callback(responseData));
+    });
+};
+
 //pulls maxPages of 100 streams each starting at the channelOffset'th stream
 //sets the next channel to one of them at random
-var getChannelList = function() {
+var getChannelList = function(callback) {
     var numResponses = 0;
     var channels = [];
     for(var page = 0; page < maxPages; ++page) {
         var offset = channelOffset + page * 100;
-        https.get('https://api.twitch.tv/kraken/streams?limit=100&offset=' + offset, function(res) {
-            var JSONResponse = '';
-            res.setEncoding('utf8');
-            res.on('data', function(chunk) {
-                JSONResponse += chunk;
+        httpsGet('https://api.twitch.tv/kraken/streams?limit=100&offset=' + offset, function(data) {
+            data = JSON.parse(data);
+            data.streams.forEach(function(stream) {
+                channels.push(stream.channel.name);
             });
-            res.on('end', function() {
-                var data = JSON.parse(JSONResponse);
-                data.streams.forEach(function(stream) {
-                    channels.push(stream.channel.name);
-                });
 
-                //when all calls return
-                if(++numResponses === maxPages) {
-                    channelList = channels;
+            //when all calls return
+            if(++numResponses === maxPages) {
+                channelList = channels;
 
-                    //set the next channel offset
-                    channelOffset = Math.floor(data._total * 0.8);
+                //set the next channel offset
+                channelOffset = Math.floor(data._total * 0.8);
+                //pick a channel if initializing
+                if(callback) {
+                    callback();
                 }
-            });
+            }
         });
     }
 };
@@ -61,18 +70,31 @@ var getChannelList = function() {
 //picks a channel out of the channelList and tells clients to change
 var changeChannel = function() {
     var index = Math.floor(Math.random() * maxPages * 100);
-    currentChannel = channelList[index];
-    //TODO: make sure this channel is still online
-    io.emit('changeChannel', currentChannel);
-    log('change ' + currentChannel);
+    var candidate = channelList[index];
+
+    //make sure this channel is still online
+    httpsGet('https://api.twitch.tv/kraken/streams/' + candidate, function(data) {
+        data = JSON.parse(data);
+        if(data.stream !== null) {
+            //send it to the clients
+            currentChannel = candidate;
+            io.emit('changeChannel', currentChannel);
+            log('change ' + currentChannel);
+        } else {
+            changeChannel();
+        }
+    });
 };
 
-//pick a channel now
-getChannelList();
-setTimeout(function() {
-    changeChannel();
-    getChannelList();
-}, 5000);
+//pick a channelOffset and channel now
+httpsGet('https://api.twitch.tv/kraken/streams?limit=1', function(data) {
+    data = JSON.parse(data);
+    channelOffset = Math.floor(data._total * 0.8);
+    getChannelList(function() {
+        changeChannel();
+        getChannelList()
+    });
+});
 
 //make and start the timekeeper for the channel changer
 var keepTime = function() {
